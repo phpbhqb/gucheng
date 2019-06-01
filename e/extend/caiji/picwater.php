608ed5beb9047d110e10f443c9457920e1a3d663
weibo <weibo@weibo.com>
2019-06-01 20:56:30 +0800

    water

---
+++ e/extend/caiji/picwater.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @Author: weibo.yao
+ * @Date:   2019-05-28 16:50:43
+ * @Last Modified time: 2019-05-29 16:06:02
+ */
+set_time_limit(0);
+ini_set('memory_limit','-1');
+date_default_timezone_set('Asia/Shanghai');
+$path=dirname(__FILE__);
+require($path.'/../../class/connect.php');
+require($path.'/../../class/db_sql.php');
+require($path.'/../../class/functions.php');
+require $path.'/../'.LoadLang("pub/fun.php");
+require($path."/../../class/t_functions.php");
+require($path.'/../../data/dbcache/class.php');
+require($path."/../../class/hinfofun.php");
+require($path."/../../class/chtmlfun.php");
+include(ECMS_PATH."e/class/gd.php");
+GetMyMarkImg(ECMS_PATH.'d/file/p/2019/05-22/586531c41d38ef0b3e94cbef0b6b36c2.jpg');
